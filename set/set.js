fb35fdaca499102ffd9f5945b646c64a8d482677
matthew-sun <527551750@qq.com>
2014-09-24 17:13:15 +0800

    set

--- set/set.js
+++ set/set.js
@@ -26,5 +26,57 @@ var Set = Class({
     },
     show : function() {
         return this.dataStore;
+    },
+    contains : function(data) {
+        return this.dataStore.indexOf(data) > -1 ? true : false;
+    },
+    // 两个集合的并集
+    union : function(set) {
+        var tempSet = Set();
+        for( var i=0; i<this.dataStore.length; i++ ) {
+            tempSet.add(this.dataStore[i]);
+        }
+        for( var i=0; i<set.dataStore.length; i++ ) {
+            if( !(tempSet.contains(set.dataStore[i])) ) {
+                tempSet.add(set.dataStore[i]);
+            }
+        }
+        return tempSet;
+    },
+    // 两个集合的交集
+    intersect : function(set) {
+        var tempSet = Set();
+        for( var i=0; i<this.dataStore.length; i++ ) {
+            if( (set.contains(this.dataStore[i])) ) {
+                tempSet.add(set.dataStore[i]);
+            }
+        }
+        return tempSet;
+    },
+    // 两个集合的补集
+    difference : function(set) {
+        var tempSet = Set();
+        for( var i=0; i<this.dataStore.length; i++ ) {
+            if( !(set.contains(this.dataStore[i])) ) {
+                tempSet.add(this.dataStore[i]);
+            }
+        }
+        return tempSet;
+    },
+    // 返回集合的长度
+    size : function() {
+        return this.dataStore.length;
+    },
+    // 判断是否是另一个集合的子集
+    subset : function(set) {
+        if( this.size() > set.size ) {
+            return false ;
+        }
+        for(var i=0,len=this.dataStore.length; i<len; i++) {
+            if( !set.contains(this.dataStore[i]) ) {
+                return false;
+            }
+        }
+        return true;
     }
 })
